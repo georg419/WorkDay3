d9f96174e982d5bf5f306cd052c09fe63984f2e6
ef <ef@gb.com>
2013-09-23 15:26:37 -0500

    Updated

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -3,20 +3,37 @@
 Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
-        Dim num1, num2, largerNum As Double
+        Dim num1, num2 As Double
+        If IsNumeric(txtFirstNum.Text) and IsNumeric(txtSecondNum.Text) Then ' if you say Or instead of And, it crashes because Ir means "if either one works, proceed"
 
-        num1 = CDbl(txtFirstNum.Text)
-        num2 = CDbl(txtSecondNum.Text)
+           
 
-        If (num1 > num2) Then
-            largerNum = num1
-            txtResult.Text = "The Larger number is " & num1
-        ElseIf (num1 < num2) Then
-            largerNum = num2
-            txtResult.Text = "The Larger number is " & num2
+            'Sterile area
+            num1 = CDbl(txtFirstNum.Text)
+            num2 = CDbl(txtSecondNum.Text)
+            If num1 < 0 Or num2 < 0 Then
+
+                MessageBox.Show("Error please type a positive number")
+            End If
+
+
+            If (num1 > num2) Then
+
+                txtResult.Text = "The Larger number is " & num1
+            ElseIf (num1 < num2) Then
+
+                txtResult.Text = "The Larger number is " & num2
+            Else
+                txtResult.Text = "They are equal"
+            End If
         Else
-            txtResult.Text = "They are equal"
+            MessageBox.Show("Error, please type a number")
+
         End If
+
+
+
+
 
 
     End Sub
